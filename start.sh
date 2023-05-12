4ffc84814b7fc08777a0498f3d480c389ba041eb
hemanthbot69 <104711832+hemanthbot69@users.noreply.github.com>
2023-05-12 22:45:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Greymattersbot/EvaMaria /EvaMaria
+  git clone https://github.com/hemanthbot69/AdvanceBot /AdvanceBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /AdvanceBot
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
